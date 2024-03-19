43e7469b701e2147bbc711bd2c71515b8c17ef66
Daniel Wei <dlw266@cornell.edu>
2024-03-19 03:47:08 -0400

    reset db on init

--- init.sql
+++ init.sql
@@ -1,5 +1,12 @@
+-- Reset the database
+DROP TABLE IF EXISTS x_docs;
+DROP TABLE IF EXISTS un_docs;
+DROP TABLE IF EXISTS topics;
+DROP TABLE IF EXISTS rel_x_topics;
+DROP TABLE IF EXISTS rel_un_topics;
+
 -- Store tweets
-CREATE TABLE IF NOT EXISTS x_docs (
+CREATE TABLE x_docs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(255),             -- The username of the tweet author
     date_created DATE,                  -- The date the tweet was created
@@ -8,7 +15,7 @@ CREATE TABLE IF NOT EXISTS x_docs (
 );
 
 -- Store UN position papers
-CREATE TABLE IF NOT EXISTS un_docs (
+CREATE TABLE un_docs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     country VARCHAR(255),               -- The country the news is from
     year_created INT,                   -- The year the news was created
@@ -16,13 +23,13 @@ CREATE TABLE IF NOT EXISTS un_docs (
 );
 
 -- Model topics for indexing and searching
-CREATE TABLE IF NOT EXISTS topics (
+CREATE TABLE topics (
     id INT AUTO_INCREMENT PRIMARY KEY,
     topic_name VARCHAR(255)            -- The name of the topic
 );
 
 -- Model many-to-many relationships between tweets and topics
-CREATE TABLE IF NOT EXISTS rel_x_topics (
+CREATE TABLE rel_x_topics (
     x_id INT,                           -- The id of the tweet
     topic_id INT,                       -- The id of the topic
     agreement FLOAT,                    -- The agreement score between the tweet and the topic, between 0 and 1 (1 indicating agreement, 0 indicating opposition)
@@ -31,7 +38,7 @@ CREATE TABLE IF NOT EXISTS rel_x_topics (
 );
 
 -- Model many-to-many relationships between UN position papers and topics
-CREATE TABLE IF NOT EXISTS rel_un_topics (
+CREATE TABLE rel_un_topics (
     un_id INT,                          -- The id of the news
     topic_id INT,                       -- The id of the topic
     agreement FLOAT,                    -- The agreement score between the news and the topic, between 0 and 1 (1 indicating agreement, 0 indicating opposition)
