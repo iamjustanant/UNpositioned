82c8fd284cc5198036e3874688450c5824a9acc3
Bingsongl <123195383+Bingsongl@users.noreply.github.com>
2024-03-22 21:06:16 -0400

    Cosine Similarity Implementation

--- backend/routes/DocSearchUN.py
+++ backend/routes/DocSearchUN.py
@@ -3,19 +3,23 @@
 # - an optional limit parameter (int) to limit the number of results, defaulting to 10
 # The function should return the most relevant UN positions.
 # Of course, the first parameter provided is the SQL Engine itself, which lets you execute SQL queries on the database.
+from nltk.sentiment import SentimentIntensityAnalyzer
+import pandas as pd
+import numpy as np
+from lib.Text_Processing_Utils import table
+from lib.Text_Processing_Utils import smart_cosdist
+
 
 def doc_search_un_handler(sql_engine,text,limit):
-  # Naive implementation here for now
-  my_sql_query = "SELECT * FROM un_docs"
-  if text:
-    my_sql_query += " WHERE text_content LIKE '%" + text + "%'"
-  if limit:
-    my_sql_query += " LIMIT " + str(limit)
-  my_sql_query += ";"
 
-  data = sql_engine.query_selector(my_sql_query)
-  return data
+  if not 'un_table' in globals():
+    global un_table
+    un_table = table(sql_engine,'un_docs')
 
+  query_vector = un_table.vectorizer.transform([str(text),]).toarray()
 
-  
-  
+  if np.array_equal(query_vector, np.zeros(shape = query_vector.shape)):
+      return ['NO FILES FOUND',]
+  else:
+    results = smart_cosdist(matrix = un_table.matrix.toarray(), query_vec = query_vector)
+    return un_table.df['text_content'].iloc[np.argsort(results)][:limit]
